99ab0f8692e238330f1a9ae5276bb23f9418a7f0
Deval1807 <deval135darji@gmail.com>
2024-12-08 12:45:46 +0530

    Update EmployeeController and Add EmployeeService
    - Add the logic for handling all requests and response in EmployeeService
    - Update EmployeeController to use EmployeeService and its methods

--- src/main/java/com/deval/ems/controller/EmployeeController.java
+++ src/main/java/com/deval/ems/controller/EmployeeController.java
@@ -1,6 +1,8 @@
 package com.deval.ems.controller;
 
 import com.deval.ems.model.Employee;
+import com.deval.ems.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,39 +11,48 @@ import java.util.List;
 @RequestMapping("/api/employees")
 public class EmployeeController {
 
+    // use the EmployeeService object to use all the methods
+    @Autowired
+    private EmployeeService employeeService;
+
     // Get all the employees
     @GetMapping()
     public List<Employee> getEmployees() {
-        System.out.println("Get all employees");
-        return null;
+//        System.out.println("Get all employees");
+//        return null;
+        return employeeService.getEmployees();
     }
 
     // Get an employee by ID
     @GetMapping("/{id}")
-    public Employee getEmployee(@PathVariable int id) {
-        System.out.println("Get an employee by ID: "+ id);
-        return null;
+    public Employee getEmployee(@PathVariable int id) throws Exception {
+//        System.out.println("Get an employee by ID: "+ id);
+//        return null;
+        return employeeService.getEmployeeById(id);
     }
 
     // Add a new employee
     @PostMapping()
     public Employee addEmployee(@RequestBody Employee newEmployee) {
-        System.out.println("New employee added");
-        System.out.println(newEmployee.toString());
-        return null;
+//        System.out.println("New employee added");
+//        System.out.println(newEmployee.toString());
+//        return null;
+        return employeeService.addEmployee(newEmployee);
     }
 
     // Edit employee detail
     @PutMapping("/{id}")
-    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee updatedEmployee) {
-        System.out.println("Update employee with ID: "+ id);
-        System.out.println(updatedEmployee.toString());
-        return null;
+    public Employee updateEmployee(@PathVariable int id, @RequestBody Employee updatedEmployee) throws Exception {
+//        System.out.println("Update employee with ID: "+ id);
+//        System.out.println(updatedEmployee.toString());
+//        return null;
+        return employeeService.editEmployee(id, updatedEmployee);
     }
 
     // Delete an employee
     @DeleteMapping("/{id}")
-    public String deleteEmployee(@PathVariable int id) {
+    public String deleteEmployee(@PathVariable int id) throws Exception {
+        employeeService.deleteEmployee(id);
         return "Employee deleted with ID: "+id;
     }
 
--- src/main/java/com/deval/ems/service/EmployeeService.java
+++ src/main/java/com/deval/ems/service/EmployeeService.java
@@ -1,4 +1,71 @@
 package com.deval.ems.service;
 
+import com.deval.ems.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class EmployeeService {
+
+    // Define a map for storing employee details
+    private Map<Integer, Employee> employeeDetails = new HashMap<>();
+    int currentId = 1;
+
+    // Get all the employees
+    public List<Employee> getEmployees() {
+        // return all the employees in an array form
+        return new ArrayList<>(employeeDetails.values());
+    }
+
+    // Get an employee by ID
+    public Employee getEmployeeById(int id) throws Exception {
+
+        // check if the ID exists
+        if(!employeeDetails.containsKey(id)) {
+            throw new Exception("Employee not found with ID: "+id);
+        }
+        return employeeDetails.get(id);
+
+    }
+
+    // Add a new employee
+    public Employee addEmployee(Employee newEmployee) {
+        // set the id to current id and add it to our hash map
+        newEmployee.setEmpId(currentId);
+        employeeDetails.put(currentId, newEmployee);
+
+        // increase the counter for hash map
+        currentId++;
+
+        return newEmployee;
+    }
+
+    // Edit an employee by id
+    public Employee editEmployee(int id, Employee updatedEmployee) throws Exception {
+        // check if the ID exists
+        if(!employeeDetails.containsKey(id)) {
+            throw new Exception("Employee not found with ID: "+id);
+        }
+
+        // Set the empId and change the entry of map corresponding to the id
+        updatedEmployee.setEmpId(id);
+        employeeDetails.put(id, updatedEmployee);
+
+        return updatedEmployee;
+    }
+
+    // Delete an employee by id
+    public void deleteEmployee(int id) throws Exception {
+        // check if the ID exists
+        if(!employeeDetails.containsKey(id)) {
+            throw new Exception("Employee not found with ID: "+id);
+        }
+
+        employeeDetails.remove(id);
+    }
+
 }
