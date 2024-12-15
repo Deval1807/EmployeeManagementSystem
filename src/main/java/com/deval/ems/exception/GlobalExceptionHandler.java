b2133a7a598f0a198155d6a97821aef50a0c5c1d
Deval1807 <deval135darji@gmail.com>
2024-12-15 11:47:11 +0530

    Add GlobalExceptionHandler class
    - Handles the request body binding exceptions and throw error on client side

---
+++ src/main/java/com/deval/ems/exception/GlobalExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.deval.ems.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//Spring annotation used to define global exception handling logic across all controllers in the application.
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    // ExceptionHandler -  of exception it will handle
+    // MethodArgumentNotValidException - when a validation error occurs during request body binding (@Valid)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // Get all the errors and add it to errors map
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}
