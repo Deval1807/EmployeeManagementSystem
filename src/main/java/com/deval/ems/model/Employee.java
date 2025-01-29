62edcf8a2c1c96d3709ee2144c6a26b7479a3db5
Deval1807 <deval135darji@gmail.com>
2025-01-29 11:24:43 +0530

    Remove JPA dependencies and annotations

--- src/main/java/com/deval/ems/model/Employee.java
+++ src/main/java/com/deval/ems/model/Employee.java
@@ -1,18 +1,15 @@
 package com.deval.ems.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor // by default @Data won't generate constructor with args
-@Entity
-@Table(name = "employees")
+@NoArgsConstructor // Needed for modelmapper
 public class Employee {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int emp_id;
     private String name;
     private int department_id;
